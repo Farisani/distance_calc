632b13c17d1124c7ebe2fa4af587e2666f16545c
Farisani <43832307+Farisani@users.noreply.github.com>
2020-06-13 23:00:29 +0200

    Update checkShortestDistance.js

--- checkShortestDistance/checkShortestDistance.js
+++ checkShortestDistance/checkShortestDistance.js
@@ -1,22 +1,29 @@
-const sortJsonDataByDistance = require('./../sortByDistance/sortByDistance.js');
+const sortJsonDataByDistance = require("./../sortByDistance/sortByDistance.js");
 
-module.exports = function checkShortestJsonData (latitude , longitude) {
-    const geoData = sortJsonDataByDistance(latitude, longitude);
+module.exports = function checkShortestJsonData(latitude, longitude) {
+  const geoData = sortJsonDataByDistance(latitude, longitude);
 
-    const checkIpAddress = geoData[0].ipv4;
+  const checkIpAddress = geoData[0].ipv4;
 
-    const data = require('./../domain/data.json');
+  const data = require("./../domain/data.json");
 
-    for (const i = 0; i < data[0].length; i++) {
-        if (checkIpAddress === getIpAddress(data[i].meta)) {
-            console.log("The shortest distance is : ")
-            return data[1];
-        }
+  for (let i = 0; i < data.length; i++) {
+    //console.log('My Ip adress for comparison',getIpAddress(data[i].meta));
+
+    let ip = getIpAddress(data[i].meta)
+    if (checkIpAddress === ip[0]) {
+      console.log("The shortest distance is : ", ip.input)
+      console.log("The shortest distance is : ",data[i])
+      
+      return data[i];
     }
+  }
 
-    return null;
-}
+  return null;
+};
 
 function getIpAddress(metaData) {
-    return metaData.match("\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b");
+  let myRegex = /\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/;
+  let ip = metaData.match(myRegex);
+  return ip;
 }
